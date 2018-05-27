5e772a08c19cd66774e6a42e955bb3f8ba252bf3
George-Patrick <patrickgeorge11@gmail.com>
2018-05-26 23:12:41 -0600

    Update Loops-Statements-Functions-Variables.js

--- Loops-Statements-Functions-Variables.js
+++ Loops-Statements-Functions-Variables.js
@@ -1,18 +1,15 @@
-
-
 //prompt for coordinates
 var coordinates = function promptForCoordinates () {
     var xCoor;
     var yCoor;
     xCoor = prompt("X Coordinates: ");
     yCoor = prompt("Y Coordinates: ");
-    return [xCoor, yCoor];
+    var lObject = {x:xCoor, y:yCoor};
+    return lObject;
 }
 
 var coor = coordinates();
-var xCoor = coor[0];
-var yCoor = coor[1];
-alert("Coordinates are as follows: " + xCoor + ", " + yCoor);
+alert("Coordinates are as follows: " + coor.x + ", " + coor.y);
 
 
 var confirm = false;
@@ -22,10 +19,8 @@ while (!confirm) {
     alert("Please enter your destination coordinates");
 
     var dCoor = coordinates();
-    var destXCoor = dCoor[0];
-    var destYCoor = dCoor[1];
 
-    alert("Destination coordinates are as follows: " + destXCoor + ", " + destYCoor);
+    alert("Destination coordinates are as follows: " + dCoor.x + ", " + dCoor.y);
     var confyes = prompt("If this is correct, please input 1.")
     
     if (confyes == 1) {
@@ -34,5 +29,8 @@ while (!confirm) {
         confirm = false;
     }
 }
+
+var locationArray = [];
+locationArray.push(dCoor)
 
 alert("Thank you for your input");
